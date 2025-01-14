47a1654d57a37df5a546094f761d959a2bd807e0
Tomer Cohen <tomixlr@gmail.com>
2025-01-14 09:35:52 +0200

    add: added all the projects and changed the original code

--- src/Components/ProjectBox.jsx
+++ src/Components/ProjectBox.jsx
@@ -1,44 +1,47 @@
 import React from 'react';
 import {FaGithub} from "react-icons/fa";
 import {CgFileDocument} from "react-icons/cg";
+import pdf from "/Verbal_Fluency_Project_Paper.pdf"
 
 
-const  ProjectBox = ({projectPhoto, projectName}) => {
+const ProjectBox = ({ projectPhoto, projectName, projectTitle, projectGithub, projectDemo }) => {
   const desc = {
+    GoalNetDesc: "A comprehensive web application for managing players, matches, and administrative tasks in the Israeli Hockey League. The platform includes scheduling tools, match tracking, league statistics, and detailed profiles for players and teams. Built with Next.js, React, Node.js, and Azure SQL",
+    GoalNetGithub: "https://github.com/Specril/Israeli-Hockey-League-Web-Application",
+    DeadLiftDesc: "Created during Global Game Jam 2024, Dead Lift is a fast-paced, two-player ragdoll game where players compete to climb as high as possible against the clock. Built from scratch in just 48 hours using Unity and won 2nd place at the competition.",
+    DeadLiftGithub: "https://github.com/dendefo/Dead-Lift",
+    DeadLiftWebsite: "https://globalgamejam.org/games/2024/dead-lift-4",
+    VFDesc:"The study investigates the differences in verbal fluency performance between native (L1) and non-native (L2) Hebrew speakers, aiming to predict language nativeness and explore the topics generated during verbal fluency tasks. We utilized Python, R, logistic regression, support vector machines (SVM), random forest algorithms, and topic modeling techniques to analyze the data.",
+    VFWebsite:pdf
+  };
 
-    GoalNetDesc:"A comprehensive web application for managing players, matches, and administrative tasks in the Israeli Hockey League. The platform includes scheduling tools, match tracking, league statistics, and detailed profiles for players and teams. Built with Next.js, React, Node.js, and SQL",
-    GoalNetGithub:"https://github.com/Specril/Israeli-Hockey-League-Web-Application",
+  const githubUrl = desc[projectName + 'Github'];
+  const websiteUrl = desc[projectName + 'Website'];
+  const description = desc[projectName + 'Desc'];
 
-    DeadLiftDesc:"Created during Global Game Jam 2024, Dead Lift is a fast-paced, two-player ragdoll game where players compete to climb as high as possible against the clock. Built from scratch in just 48 hours using Unity and won 2nd place at the competition.",
-    DeadLiftGithub:"https://github.com/dendefo/Dead-Lift",
-    DeadLiftWebsite:"https://globalgamejam.org/games/2024/dead-lift-4"
-  }
-
-  let show ='';
-  if(desc[projectName + 'Github']===""){
-    show="none";
-  }
-    
   return (
-    <div className='projectBox'> 
-        <img className='projectPhoto' src={projectPhoto} alt="Project display" /> 
-        <div>
-            <br />
-            <h3>{projectName}</h3>
-            <br />
-            {desc[projectName + 'Desc']}
-            <br />
-
-            <a style={{display:show}} href={desc[projectName + 'Github']} target='_blank'>
-              <button className='projectbtn'><FaGithub/> Github</button>
-            </a>
-
-            <a href={desc[projectName + 'Website']} target='_blank'>
-              <button className='projectbtn'><CgFileDocument/> Demo</button>
-            </a>
-        </div>
+    <div className='projectBox'>
+      <img className='projectPhoto' src={projectPhoto} alt="Project display" />
+      <div>
+        <br />
+        <h3>{projectTitle}</h3>
+        <br />
+        <p>{description}</p>
+        <br />
+        {projectGithub && githubUrl && (
+          <a href={githubUrl} target='_blank'>
+            <button className='projectbtn'><FaGithub /> Github</button>
+          </a>
+        )}
+        {projectDemo && websiteUrl && (
+          <a href={websiteUrl} target='_blank' download="Verbal Fluency Project">
+            <button className='projectbtn'><CgFileDocument /> Demo</button>
+          </a>
+        )}
+      </div>
     </div>
-  )
-}
+  );
+};
+
 
 export default  ProjectBox
--- src/Components/Projects.jsx
+++ src/Components/Projects.jsx
@@ -2,14 +2,16 @@ import React from 'react';
 import ProjectBox from './ProjectBox';
 import HockeyImage from '../images/GoalNetLaptop.png'
 import DeadLiftImage from '../images/DeadLiftLaptop.png'
+import VFImage from '../images/VFLaptop.png'
 
 const Projects = () => {
   return (
     <div>
       <h1 className='projectHeading'>My <b>Projects</b></h1>
       <div className='project'>
-        <ProjectBox projectPhoto={HockeyImage} projectName="GoalNet" />
-        <ProjectBox projectPhoto={DeadLiftImage} projectName="DeadLift" />
+        <ProjectBox projectPhoto={HockeyImage} projectName="GoalNet" projectGithub={true} projectDemo={false} projectTitle={"Goal-Net: Israeli Hockey Website"}/>
+        <ProjectBox projectPhoto={DeadLiftImage} projectName="DeadLift" projectGithub={true} projectDemo={true} projectTitle={"Dead Lift"}/>
+        <ProjectBox projectPhoto={VFImage} projectName="VF" projectGithub={true} projectDemo={true} projectTitle={"Verbal Fluency NLP Study"}/>
       </div>
 
     </div>
